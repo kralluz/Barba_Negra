2c1cf769847f8f8dbc61a16a7a55af6955f26a13
Carlos Henrique <97392902+kralluz@users.noreply.github.com>
2023-10-07 16:43:02 -0300

    upload logica
    a

---
+++ Barbearia/src/main/java/com/mycompany/barbearia/banco/Banco.sql
@@ -0,0 +1,8 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  alexandre
+ * Created: Sep 30, 2023
+ */
---
+++ logicCarlos/README.md
@@ -0,0 +1,2 @@
+# BARBEARIA BARBA NEGRA
+> O projeto visa criar um banco de dados para um sistema de gerenciamento de horários de cortes de cabelo, atendendo varios clientes, em vários horários.
